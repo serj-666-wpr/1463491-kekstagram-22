03085b54fdec31714b5f88be21257957cc122595
Сергей <segaga18@rambler.ru>
2021-03-24 20:27:49 +0300

    Исправление ошибок

--- source/js/form.js
+++ source/js/form.js
@@ -1,20 +1,13 @@
 import { sendForm } from './api.js';
 import { showSuccess } from './message.js';
-import { validHashtags } from './download.js';
 
 const form = document.querySelector('.img-upload__form');
-const hashtagInput = document.querySelector('.text__hashtags');
 
 const addImageSubmitHandler = () => {
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    const valid = validHashtags();
-    if (valid) {
-      hashtagInput.style.outline = '2px solid red';
-    } else {
-      const formData = new FormData(form);
-      sendForm(formData, showSuccess);
-    }
+    const formData = new FormData(form);
+    sendForm(formData, showSuccess);
   });
 }
 
